8d0ba36701a0813da35441908f09c8d1d1249847
unknown <maryamihsan601@gmail.com>
2024-03-01 23:51:33 +0300

    fav-song

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
+
+
+
 function displaySongInfo() {
+    let display = document.getElementById('info');
+    let Sname = document.getElementById("SongName").value;
+   
+    let link = document.getElementById("YouTubeLink").value;
+    let iframe = document.getElementById("YouTubeLink");
+    let Discription = document.getElementById("dicription").value;
+    let y = document.getElementById("year").value;
+     iframe.src = link
+    display.style.visibility = 'visible';
+   
+    document.getElementById("sname").innerHTML = Sname;
+   // document.getElementById("YouTube").innerHTML =vedio ;
+    document.getElementById("dis").innerHTML = Discription;
+    document.getElementById("songYear").innerHTML = y;
+   
 
 }
+document.getElementById("song").addEventListener("submit" , function(event){
+    event.preventDefault();
+    displaySongInfo();
+})
 module.exports = {
     displaySongInfo: displaySongInfo,
 };
